df5636189fa1383b072d3681c9c92b25435ff9ba
unknown <rsfbernardes@hotmail.com>
2021-01-26 21:36:34 -0300

    Correcao da pasta dos links e adicao de um script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-function codigo_service() {
+/* script para alterar o texto do botão "Código do serviço" */
+function service_code () {
     document.getElementById("botao-rodape").innerHTML = "123-456"
+}
+
+/* script para colocar uma interatividade no ícone do presente */
+function gift () {
+    window.alert("Você ganhou um mês de assinatura grátis!")
 }
